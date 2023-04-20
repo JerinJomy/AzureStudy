f11601fe8c2d9d02f31011f6c6fb2f793dc4eb52
Jerin <jerinjomy123@outlook.com>
2023-04-20 12:42:26 +0530

    feat: Intitial commit

---
+++ SampleWebApp/Controllers/WeatherForecastController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SampleWebApp.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SampleController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<SampleController> _logger;
+
+    public SampleController(ILogger<SampleController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "Value")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+}
+
